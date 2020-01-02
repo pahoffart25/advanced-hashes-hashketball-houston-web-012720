1ca39860bef40e959aa9183965e1c0f9a45de5ac
Paul Hoffart <pahoffart25@gmail.com>
2020-01-02 22:02:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -217,7 +217,7 @@ def player_with_longest_name
   name_length = 0
   game_hash.each do |team, team_info|
     team_info [:players].each do |player|
-      if [:name] < name_length
+      if [:name].length < name_length
         name_length = player[:name].length
         player_with_long_name = player[:name]
       end
